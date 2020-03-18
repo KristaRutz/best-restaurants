ec184dc46a285f2ea8d572eaa71b3e985b6c596d
Krista Rutz <57574670+KristaRutz@users.noreply.github.com>
2020-03-18 11:02:24 -0700

    Initial commit

---
+++ Project/Models/ProjectNameContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectName.Models
+{
+  public class ProjectNameContext : DbContext
+  {
+    public virtual DbSet<Category> Categories { get; set; }
+    public DbSet<Item> Items { get; set; }
+
+    public ProjectNameContext(DbContextOptions options) : base(options) { }
+  }
+}
